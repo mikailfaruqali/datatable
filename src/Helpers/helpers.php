e5797e827782d1232e1ce0709e9406b5df94c0bf
Snawbar <alanfaruq85@gmail.com>
2025-06-15 12:20:18 +0300

    Refactor helper functions to remove type hints for parameters in asset_or_url and datatable_when

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -7,7 +7,7 @@ function datatableProcess($datatables): DatatableProcess
     return new DatatableProcess($datatables);
 }
 
-function asset_or_url(?string $path)
+function asset_or_url($path)
 {
     if (blank($path)) {
         return NULL;
@@ -16,7 +16,7 @@ function asset_or_url(?string $path)
     return filter_var($path, FILTER_VALIDATE_URL) ? $path : asset($path);
 }
 
-function datatable_when($condition, callable|string $true, callable|string|null $false = NULL): string
+function datatable_when($condition, $true, $false = NULL): string
 {
     $result = $condition ? $true : $false;
 
