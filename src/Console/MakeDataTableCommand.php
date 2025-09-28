5151a6181cd1be2501e7a5fe68e0b5d85e808089
Mikail Faruq <alanfaruq85@gmail.com>
2025-09-28 23:20:53 +0300

    feat: Add MakeDataTableCommand for generating DataTable classes

---
+++ src/Console/MakeDataTableCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Snawbar\DataTable\Console;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeDataTableCommand extends GeneratorCommand
+{
+    protected $signature = 'make:datatable {name}';
+
+    protected function getStub()
+    {
+        return __DIR__ . '/../../stubs/datatable.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return sprintf('%s\\DataTables', $rootNamespace);
+    }
+}
